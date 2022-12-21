aeb1beced1a51a4802e3c400fdcb462c8377500d
Arthur Phillipe <arthurphillipe@yahoo.com.br>
2022-12-21 11:48:47 -0300

    Atualização dos arquivos para entrega do Projeto Final ao Senai

--- Entities/Piloto.cs
+++ Entities/Piloto.cs
@@ -11,5 +11,6 @@ public class Piloto
     public int Id { get; set; }
     public string Nome { get; set; }
     public string Matricula { get; set; }
+    // public string Breve { get; set; }  COMENTÁRIO: PARA incluir precisa atualizar as demais partes do código
     public ICollection<Voo> Voos { get; set; } = null!;
 }
